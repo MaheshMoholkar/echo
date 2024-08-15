75526f3c41643e9f8aa683d6a3a23e334df605b8
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-08-15 12:09:52 +0530

    initial commit

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,10 @@
+import { relations } from "drizzle-orm";
+import { serial, text, timestamp, integer, pgTable } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
