e9a2095721f038645cd85426251e1506639956e6
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-08-16 11:34:00 +0530

    remove files

--- backend/src/db/migrations/20240816055905_chief_gertrude_yorkes.sql
+++ backend/src/db/migrations/20240816055905_chief_gertrude_yorkes.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"profileImage" text,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
--- frontend/src/state/api.ts
+++
@@ -1,9 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5173" }),
-  reducerPath: "api",
-  tagTypes: [],
-  endpoints: () => ({}),
-});
-
-export const {} = api;
